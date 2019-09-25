17909d06479fd5617fe6e1630bb191e26affd3b4
Patrick <pjm42085@creighton.edu>
2019-09-25 15:46:17 -0500

    Initial commit

---
+++ forms/larger/larger.js
@@ -0,0 +1,20 @@
+//Needs to be another quote around "Enter a number."
+//promp needs to be prompt
+//Both vars should be let
+let firstNum = prompt("Enter a number.")
+//Prompt needs to be prompt
+let secondNum = prompt("Enter another number.")
+//10 and firstNum need to be converted to numbers
+firstNum = Number(firstNum) + Number('10')
+//9 and secondNum need to be converted to numbers
+//secondnum needs to be secondNum
+secondNum = Number(secondNum) + Number('9')
+//Need () around if statament
+//<= needs to be >=
+if (firstNum >= secondNum)
+    //console.log needs to be an alert
+    alert('The first number is larger than the second.')
+  else
+    //console.log needs to be an alert
+    //` needs to be changed to "
+    alert("The second number is larger than the first.")
